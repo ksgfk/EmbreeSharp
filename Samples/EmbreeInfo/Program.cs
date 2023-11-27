449cc9eacc6aee17a0c089e7e2b3f60edadaa485
ksgfk <1537100607@qq.com>
2023-11-28 00:39:08 +0800

    example

---
+++ Samples/EmbreeInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using EmbreeSharp.Native;
+using System.Text;
+
+internal class Program
+{
+    private static unsafe void Main(string[] args)
+    {
+        string config = "verbose=1";
+        var bytes = Encoding.UTF8.GetBytes(config);
+        fixed (byte* ptr = bytes)
+        {
+            RTCDevice device = GlobalFunctions.rtcNewDevice(ptr);
+            GlobalFunctions.rtcReleaseDevice(device);
+        }
+    }
+}
