5c0ff476ba23a1d20aae7580dc14111896a14322
ksgfk <1537100607@qq.com>
2024-06-15 11:29:24 +0800

    update embree

--- Script/pack-native.ps1
+++ Script/pack-native.ps1
@@ -1,4 +1,4 @@
-$EmbreeVersion = "4.3.1"
+$EmbreeVersion = "4.3.2"
 $OutputDir = "build"
 $NugetUri = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $NugetExe = "$OutputDir/nuget.exe"
@@ -18,3 +18,4 @@ Invoke-WebRequest -Uri $NugetUri -OutFile $NugetExe
 .\pack-native-win-x64.ps1 $EmbreeVersion $OutputDir $NugetExe
 .\pack-native-linux-x64.ps1 $EmbreeVersion $OutputDir $NugetExe
 .\pack-native-osx-x64.ps1 $EmbreeVersion $OutputDir $NugetExe
+.\pack-native-osx-arm64.ps1 $EmbreeVersion $OutputDir $NugetExe
